1926ebcdb4aa5c1bc49c738024d9281444ee48e9
Shubhankar Kotnala <skshubhankar@gmail.com>
2019-11-28 02:21:25 +0530

    Initial Commit

--- app/src/main/java/io/github/shubhkotnala/walkthrough/WalkthroughDemo.java
+++ app/src/main/java/io/github/shubhkotnala/walkthrough/WalkthroughDemo.java
@@ -8,10 +8,10 @@ public class WalkthroughDemo extends WalkthroughActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setPageTitle("Testing");
+        setPageTitle("Walkthrough");
         addWalkthroughItem(new WalkthroughItem(R.drawable._follow, "#7BC8C0","Walkthrough","A library that will generate the Walkthrough of your app without any pain.\n Supports links in the description, long text.\nNow you can focus on your main app development rather than the Introduction.\nA small and simple library which is all you need"));
         addWalkthroughItem(new WalkthroughItem(R.drawable.lorem, "#967ADC","Supports Long Description",getResources().getString(R.string.lorem).toString()));
-        addWalkthroughItem(new WalkthroughItem(R.drawable.github_octocat, "#000000","Download","Download this project on Github http://github.com/ShubhKotnala"));
+        addWalkthroughItem(new WalkthroughItem(R.drawable.github_octocat, "#000000","Download","Download this project on Github\nhttp://github.com/ShubhKotnala"));
         addWalkthroughItem(new WalkthroughItem(R.drawable.success, "#4CAF50","You're all set!","Slide more to get this finished or press the button at the bottom of this card to finish the Walkthrough and enter your amazing app."));
 
         startWalkthrough();
